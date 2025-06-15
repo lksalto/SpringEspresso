cffdf40f3d06cf8219a3c5705b4b488c79bb5b66
ronanpjr <ronanpereira@estudante.ufscar.br>
2025-06-15 17:08:54 -0300

    trecos

--- src/test/java/br/ufscar/dc/dsw/EspressoGamingTestingJpaApplicationTests.java
+++ src/test/java/br/ufscar/dc/dsw/EspressoGamingTestingJpaApplicationTests.java
@@ -2,7 +2,9 @@ package br.ufscar.dc.dsw;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(locations = "classpath:application.properties")
 @SpringBootTest
 class EspressoGamingTestingJpaApplicationTests {
 
